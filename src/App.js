9a5ff419a27b924040c4f5bab4b5fcc6c0929d69
ptthao0801 <ptthao0801@gmail.com>
2024-07-09 16:17:48 +0700

    first cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Welcome to React Router!</h1>
+          <Routes>
+              <Route path='/' element={<Home/> } />
+              <Route path='about' element={<About />} />
+          </Routes>
     </div>
   );
 }
 
 export default App;
+
+function Home() {
+    return (
+        <>
+            <main>
+                <h2>Welcome to the homepage!</h2>
+            </main>
+            <nav>
+                <Link to="/about">About</Link>
+            </nav>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <main>
+                <h2>Who are we?</h2>
+                <p>
+                    React Router is a lightweight, fully-featured routing library for the React JavaScript library
+                </p>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </>
+    );
+}
